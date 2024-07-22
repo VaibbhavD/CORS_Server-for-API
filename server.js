b1411cbb2e635e69967ef4c93036283cbb1fb06f
Vaibhav D <vaibhavdhamanage12@gmail.com>
2024-07-22 16:27:39 +0530

    Update Srver

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// server.js
 const corsAnywhere = require("cors-anywhere");
 const express = require("express");
 const app = express();
@@ -11,6 +10,8 @@ app.use((req, res, next) => {
     "GET, POST, PUT, DELETE, OPTIONS"
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Max-Age", "86400"); // Cache preflight request for 1 day
+
   if (req.method === "OPTIONS") {
     res.sendStatus(200);
   } else {
@@ -19,15 +20,18 @@ app.use((req, res, next) => {
 });
 
 // Create and start CORS Anywhere server
-corsAnywhere
-  .createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"],
-    // Redirects are automatically followed by default
-    // If you have specific needs, you can adjust the maxRedirects option here
-    // maxRedirects: 5 // Optional: default is 5 redirects
-  })
-  .listen(process.env.PORT || 8080, "0.0.0.0", () => {
-    console.log("CORS Anywhere server running");
-  });
+const corsServer = corsAnywhere.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ["origin", "x-requested-with"],
+  removeHeaders: ["cookie", "cookie2"],
+  // Disable following redirects for preflight requests
+  maxRedirects: 0,
+});
+
+app.use((req, res) => {
+  corsServer.emit("request", req, res);
+});
+
+app.listen(process.env.PORT || 8080, "0.0.0.0", () => {
+  console.log("CORS Anywhere server running");
+});
